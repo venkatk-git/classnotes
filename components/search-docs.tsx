1b799a87f66aef9b0a11ce5de7aefb9b7e359c7c
venkatk-git <venkatkumar.profiles@gmail.com>
2024-10-16 23:46:29 +0530

    Added Global Search field

--- components/search-docs.tsx
+++ components/search-docs.tsx
@@ -1,21 +1,92 @@
+"use client";
+
+import React from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
+import {
+    CommandDialog,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "./ui/command";
+import { DialogTitle } from "./ui/dialog";
+import { docsConfig } from "@/config/docs";
+import { useRouter } from "next/navigation";
+import { FileIcon } from "lucide-react";
 
 export function Search() {
+    const router = useRouter();
+    const [open, setOpen] = React.useState(false);
+
+    // Ctrl + K => Open command menu
+    React.useEffect(() => {
+        function down(e: KeyboardEvent) {
+            if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+                e.preventDefault();
+                setOpen(true);
+                console.log("pressed");
+            }
+        }
+
+        document.addEventListener("keydown", down);
+
+        return () => document.removeEventListener("keydown", down);
+    }, [open]);
+
+    function navigateCommand(link: string) {
+        setOpen(false);
+        router.push(link);
+    }
+
     return (
-        <Button
-            variant="outline"
-            className={cn(
-                "relative h-8 w-full mr-2 justify-start hidden xs:flex rounded-[0.5rem] bg-accent text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-64 border-0"
-            )}
-        >
-            <span className="hidden lg:inline-flex">
-                Search documentation...
-            </span>
-            <span className="w-14 inline-block lg:hidden">Search...</span>
-            <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded dark:bg-gray-700 shadow-sm px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-                <span className="text-xs">⌘</span>K
-            </kbd>
-        </Button>
+        <>
+            <Button
+                variant="outline"
+                className={cn(
+                    "relative h-8 w-full mr-2 justify-start hidden xs:flex rounded-[0.5rem] bg-accent text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-64 border-0"
+                )}
+                onClick={() => setOpen(true)}
+            >
+                <span className="hidden lg:inline-flex">
+                    Search documentation...
+                </span>
+                <span className="w-14 inline-block lg:hidden">Search...</span>
+                <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded dark:bg-gray-700 shadow-sm px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+                    <span className="text-xs">⌘</span>K
+                </kbd>
+            </Button>
+            <CommandDialog open={open} onOpenChange={setOpen}>
+                <DialogTitle asChild>
+                    <span className="sr-only">Search documentation</span>
+                </DialogTitle>
+                <CommandInput placeholder="Type a command or search..." />
+                <CommandList>
+                    <CommandEmpty>No results found :(</CommandEmpty>
+                    <CommandGroup heading="Java">
+                        {docsConfig.javaDocs.map((group) => (
+                            <CommandGroup
+                                key={group.title}
+                                heading={group.title}
+                            >
+                                {group.items.map((item) => (
+                                    <CommandItem
+                                        key={item.href}
+                                        value={item.title}
+                                        onSelect={() =>
+                                            navigateCommand(item.href)
+                                        }
+                                    >
+                                        <FileIcon className="mr-2 h-4 w-4" />
+                                        <span>{item.title}</span>
+                                    </CommandItem>
+                                ))}
+                            </CommandGroup>
+                        ))}
+                    </CommandGroup>
+                </CommandList>
+            </CommandDialog>
+        </>
     );
 }
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -2,10 +2,11 @@ import { siteConfig } from "@/config/site";
 import { Icons } from "./icons";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import { Button, buttonVariants } from "./ui/button";
+import { buttonVariants } from "./ui/button";
 import { MainNav } from "./main-nav";
 import { MobileNav } from "./mobile-nav";
 import { ModeToggle } from "./mode-toggle";
+import { Search } from "./search-docs";
 
 export function SiteHeader() {
     return (
@@ -14,22 +15,7 @@ export function SiteHeader() {
                 <MainNav />
                 <div className="flex flex-1 items-center justify-end space-x-2 lg:ml-6">
                     <nav className="flex items-center space-x-2">
-                        <Button
-                            variant="outline"
-                            className={cn(
-                                "relative h-8 w-full mr-2 justify-start hidden xs:flex rounded-[0.5rem] bg-accent text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-64 border-0"
-                            )}
-                        >
-                            <span className="hidden lg:inline-flex">
-                                Search documentation...
-                            </span>
-                            <span className="w-14 inline-block lg:hidden">
-                                Search...
-                            </span>
-                            <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded dark:bg-gray-700 shadow-sm px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-                                <span className="text-xs">⌘</span>K
-                            </kbd>
-                        </Button>
+                        <Search />
                         <Link
                             href={siteConfig.links.github}
                             target="_blank"
