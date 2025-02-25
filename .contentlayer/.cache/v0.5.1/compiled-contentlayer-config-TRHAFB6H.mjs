8fc6f9e610cd22bdd05db97b4867a1d107d0bda2
venkatk-git <venkatkumar.profiles@gmail.com>
2025-02-25 19:09:30 +0530

    build fix

---
+++ .contentlayer/.cache/v0.5.1/compiled-contentlayer-config-TRHAFB6H.mjs
@@ -0,0 +1,150 @@
+// contentlayer.config.js
+import { defineDocumentType, makeSource } from "contentlayer2/source-files";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+var computedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => `/${doc._raw.flattenedPath}`
+  },
+  slugAsParams: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/")
+  }
+};
+var Java = defineDocumentType(() => ({
+  name: "Java",
+  filePathPattern: `java/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    author: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string",
+      required: true
+    },
+    prerequisites: {
+      type: "list",
+      of: {
+        type: "string"
+      }
+    },
+    next: {
+      type: "string",
+      required: false
+    },
+    previous: {
+      type: "string",
+      required: false
+    }
+  },
+  computedFields
+}));
+var React = defineDocumentType(() => ({
+  name: "React",
+  filePathPattern: `react/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    author: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string",
+      required: true
+    },
+    prerequisites: {
+      type: "list",
+      of: {
+        type: "string"
+      }
+    },
+    next: {
+      type: "string",
+      required: false
+    },
+    previous: {
+      type: "string",
+      required: false
+    }
+  },
+  computedFields
+}));
+var OS = defineDocumentType(() => ({
+  name: "OS",
+  filePathPattern: `os/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    author: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string",
+      required: true
+    },
+    prerequisites: {
+      type: "list",
+      of: {
+        type: "string"
+      }
+    },
+    next: {
+      type: "string",
+      required: false
+    },
+    previous: {
+      type: "string",
+      required: false
+    }
+  },
+  computedFields
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "content",
+  documentTypes: [Java, React, OS],
+  mdx: {
+    remarkPlugins: [],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          keepBackground: false
+        }
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section"
+          }
+        }
+      ]
+    ]
+  }
+});
+export {
+  Java,
+  OS,
+  React,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-TRHAFB6H.mjs.map
